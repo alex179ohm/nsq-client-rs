654d252514897b304f966c9f013d39996cb947ea
alex179ohm <alex179ohm@gmail.com>
2019-02-15 19:09:47 +0100

    Add consumer and reader examples

---
+++ examples/producer.rs
@@ -0,0 +1,54 @@
+// MIT License
+// 
+// Copyright (c) 2019-2021 Alessandro Cresto Miseroglio <alex179ohm@gmail.com>
+// Copyright (c) 2019-2021 Tangram Technologies S.R.L. <https://tngrm.io>
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+extern crate nsqueue;
+extern crate actix;
+extern crate futures;
+
+use actix::prelude::*;
+use std::time::Duration;
+use nsqueue::{Producer, Pub, Config};
+
+struct MyClient ( Addr<Producer> );
+
+impl Actor for MyClient {
+    type Context = Context<Self>;
+    fn started(&mut self, ctx: &mut Self::Context) {
+        let dur = Duration::from_secs(1);
+        ctx.run_later(dur, |act, _ctx| {
+            for i in 0..200 {
+                act.0.do_send(Pub(format!("test {} msg sent", i)));
+            }
+        });
+    }
+}
+
+fn main() {
+    let config = Config::default().client_id("Producer".to_owned());
+    System::run(|| {
+        let p = Supervisor::start(|_|
+            Producer::new("test", "test", "0.0.0.0:4150", Some(config), "")
+        );
+        MyClient(p).start();
+    });
+}
---
+++ examples/reader/src/main.rs
@@ -0,0 +1,69 @@
+// MIT License
+// 
+// Copyright (c) 2019-2021 Alessandro Cresto Miseroglio <alex179ohm@gmail.com>
+// Copyright (c) 2019-2021 Tangram Technologies S.R.L. <https://tngrm.io>
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+extern crate nsq_client;
+extern crate actix;
+
+use std::sync::Arc;
+
+use actix::prelude::*;
+
+use nsq_client::{Connection, Msg, Fin, Subscribe, Config};
+
+struct MyReader(pub Arc<Addr<Connection>>);
+
+
+impl Actor for MyReader {
+    type Context = Context<Self>;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        self.subscribe::<Msg>(ctx, self.0.clone());
+    }
+}
+
+impl Handler<Msg> for MyReader {
+    type Result = ();
+    // on identify
+    fn handle(&mut self, msg: Msg, _ctx: &mut Self::Context) {
+        println!("MyReader: {:?}", msg);
+        self.0.do_send(Fin(msg.id));
+    }
+}
+
+
+fn main() {
+    env_logger::init();
+    let sys = System::new("nsq-consumer");
+    let config = Config::default().client_id("consumer".to_string());
+    let c = Supervisor::start(|_| Connection::new(
+            "test", // topic
+            "test", //channel
+            "0.0.0.0:4150", //nsqd tcp address
+            Some(config), //config (Optional see mod config for defaults, if None Consumer sets defaults)
+            Some(2), // rdy (optional if None set rdy to 1)
+    ));
+    let conn = Arc::new(c);
+    let _ = MyReader(conn.clone()).start(); // same thread reader
+    let _ = Arbiter::start(move |_| MyReader(conn)); // other thread reader
+    sys.run();
+}
