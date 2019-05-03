4a8fa6289e9f8af17b938e86c80af7a29323c200
alex179ohm <alex179ohm@gmail.com>
2019-05-03 16:36:19 +0200

    Fix conn update

--- src/conn.rs
+++ src/conn.rs
@@ -388,13 +388,13 @@ pub fn connect(addr: &str, buffer_size: usize) -> TcpStream {
         info!("{:?}", addr);
         match TcpStream::connect(&addr) {
             Ok(stream) => {
-                if let Err(e) = stream.peer_addr() {
-                    info!("error on connection: {}", e);
-                    if let Some(timeout) = backoff.next_backoff() {
-                        thread::sleep(timeout);
-                    }
-                    continue
-                }
+                //if let Err(e) = stream.peer_addr() {
+                //    info!("error on connection: {}", e);
+                //    if let Some(timeout) = backoff.next_backoff() {
+                //        thread::sleep(timeout);
+                //    }
+                //    continue
+                //}
                 info!("[{}] connected", addr);
                 let _ = stream.set_recv_buffer_size(buffer_size);
                 return stream;
