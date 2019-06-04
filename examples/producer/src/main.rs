9e1102c854124528a220819564bb9149903899b6
alex179ohm <alex179ohm@gmail.com>
2019-06-04 15:20:03 +0200

    new producer example

--- examples/producer/src/main.rs
+++ examples/producer/src/main.rs
@@ -1,18 +1,31 @@
-use nsq_rust::{Client, Context, Pub, NsqCmd};
-use log::info;
+use nsq_client::{Client, Context, Pub, Config, Producer, VerifyServerCert};
+use std::time::Duration;
+use std::thread;
 use env_logger;
-use std::time::{Instant, Duration};
+
+#[derive(Copy, Clone, Debug)]
+struct MyProducer;
+
+impl Producer for MyProducer {
+    fn send(&mut self, ctx: &mut Context) {
+        thread::sleep(Duration::from_millis(1000));
+        ctx.send(Pub("test".to_owned(), "message".to_owned()));
+    }
+}
 
 fn main() {
     env_logger::init();
-    let mut c = Client::new("0.0.0.0:4150", "test", "test", 3, None);
-    c.with_producer();
+    let mut config: Config<String> = Config::default();
+    config.tls(VerifyServerCert::None);
+    let mut c = Client::new(
+        "test",
+        "test",
+        "localhost:4150",
+        config,
+        None,
+        0,
+        0,
+    );
+    c.spawn_producer(4, MyProducer{});
     c.run();
-    let p = c.producer();
-
-    let now = Instant::now();
-    for i in 0..1_000_000 {
-        p.send(Pub("test".to_owned(), format!("msg {} sent", i)).as_cmd());
-    }
-    println!("time: {:#?}", now - Instant::now());
 }
