f6bf60916c604803251b8fc117471bbf504bea6e
Alessandro Cresto Miseroglio <alex179ohm@gmail.com>
2019-07-31 18:34:48 +0200

    Fix.

--- src/client.rs
+++ src/client.rs
@@ -172,11 +172,9 @@ where
                                 warn!("socket would block");
                                 #[cfg(target_os = "windows")]
                                 {
-                                    loop {
-                                        let buf = &mut [0; 32];
-                                        let ret = res.get_ref().write(buf);
-                                        debug!("readed: {:?}, {:?}", ret, buf);
-                                    }
+                                    let buf = &mut [0; 32];
+                                    let ret = res.get_ref().write(buf);
+                                    debug!("written: {:?}, {:?}", ret, buf);
                                 }
                                 #[cfg(not(target_os = "windows"))]
                                 thread::sleep(Duration::from_millis(1000));
